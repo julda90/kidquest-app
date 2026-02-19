ccf7632fb8ab72d62b4d9b49f6168c9ee1252d4a
julda90 <julda@hvt.cz>
2026-02-19 20:47:51 +0100

    chore: initial spring boot project scaffold

---
+++ src/main/java/com/KidQuest/kidquest_app/KidquestAppApplication.java
@@ -0,0 +1,13 @@
+package com.KidQuest.kidquest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidquestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidquestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KidQuest/kidquest_app/KidquestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KidQuest.kidquest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidquestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
