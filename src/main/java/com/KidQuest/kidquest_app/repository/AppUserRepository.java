a5c01413ba1a3d0e8cc34cb42c93af2740420c39
julda90 <julda@hvt.cz>
2026-03-10 18:50:11 +0100

    Update: AppUserRepository parameter Long -> UUID

--- src/main/java/com/KidQuest/kidquest_app/repository/AppUserRepository.java
+++ src/main/java/com/KidQuest/kidquest_app/repository/AppUserRepository.java
@@ -4,8 +4,9 @@ import com.KidQuest.kidquest_app.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
 
     Optional<AppUser> findByEmail(String email);
 
